acc7806b26665c75eda535787665cdd037599e3d
Pavel Shapel <p.shapel@gmail.com>
2022-06-24 17:01:39 +0300

    increase JWT expiration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '10m' },
     }),
   ],
   providers: [
