cdf873ae9f4a63ed4a8417cb1fd64e1a4a8b4f9b
Pavel Shapel <p.shapel@gmail.com>
2022-06-24 14:59:38 +0300

    clean up

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { DatabaseModule } from '../database/database.module';
-import { usersProviders } from './users.providers';
+import { USER_MODEL } from './constants';
+import { UserDocument, UserSchema } from './schemas/user.schema';
+import { DATABASE_CONNECTION } from '../database/constants';
+import { Mongoose } from 'mongoose';
+
 @Module({
   imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [UsersService, ...usersProviders],
+  providers: [
+    UsersService,
+    {
+      provide: USER_MODEL,
+      useFactory: (mongoose: Mongoose) =>
+        mongoose.model(UserDocument.name, UserSchema),
+      inject: [DATABASE_CONNECTION],
+    },
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
