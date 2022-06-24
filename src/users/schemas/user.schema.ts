e286dc5bd07d6d1c7a48a46e834fd6c33305a435
Pavel Shapel <p.shapel@gmail.com>
2022-06-24 15:42:35 +0300

    configure users schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -2,8 +2,22 @@ import { Prop, Schema } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { SchemaFactory } from '@nestjs/mongoose';
 import * as passportLocalMongoose from 'passport-local-mongoose';
+import { User } from '../interfaces/user.interface';
 
-@Schema()
+const transform = (document: UserDocument): User => {
+  return {
+    id: document._id,
+    email: document.email,
+    firstName: document.firstName,
+    lastName: document.lastName,
+  };
+};
+@Schema({
+  collection: 'shapel_users',
+  timestamps: true,
+  toObject: { transform },
+  toJSON: { transform },
+})
 export class UserDocument extends Document {
   @Prop({ _id: true })
   id: string;
