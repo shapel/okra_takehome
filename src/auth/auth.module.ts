7d48bf7371021f2a90195f9ce32df8760559f61b
Pavel Shapel <p.shapel@gmail.com>
2022-06-25 16:04:13 +0300

    fix auth misconfiguration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import configuration from '../common/configuration';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
@@ -10,10 +12,11 @@ import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
+    ConfigModule,
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: 'secret',
+      secret: configuration().JWT_SECRET,
       signOptions: { expiresIn: '10m' },
     }),
   ],
